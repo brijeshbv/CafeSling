1ae812f87a64b5c68a4867301c67892476da9d26
brijeshbv <brijesh.163@gmail.com>
2016-07-21 14:59:57 +0530

    my new branch

--- app/src/main/java/com/example/brijeshvishwanat/cafesling/MainActivity.java
+++ app/src/main/java/com/example/brijeshvishwanat/cafesling/MainActivity.java
@@ -27,13 +27,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-// hey gautham
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+    // hey gautham
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
